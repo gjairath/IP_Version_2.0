1a8bc79467a4e77f4ed0619751848c81fcc7ea22
ajay jairath <gjairath@purdue.edu>
2021-05-31 14:22:27 -0400

    Deploy

--- ip_app/__init__.py
+++ ip_app/__init__.py
@@ -7,7 +7,8 @@ Created on Sun May 16 02:33:24 2021
 
 # File imports
 from ip_app.config import Config
-
+import os
+import sys
 
 # Packages
 from flask import Flask
@@ -18,7 +19,14 @@ from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-app.config.from_object(Config)
+
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
+
+print ("\n\n\n\n{}".format(uri), file=sys.stdout)
 
 login = LoginManager(app)
 # Set login view to make a wall user shall not pass.
